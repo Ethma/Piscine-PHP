3849bb614e40800c3694f8f17bf64ef69d318f43
mabessir <mabessir@student.42.fr>
2018-03-27 21:15:21 +0200

    Finished day01

--- Day01/ex13/sing_it!.php
+++ Day01/ex13/sing_it!.php
@@ -1,10 +1,5 @@
 #!/usr/bin/php
 <?PHP
-	if ($argc == 1)
-	{
-		echo "\n";
-		return (0);
-	}
-	if ($argv[1] == "mais pourquoi cette demo ?")
-		echo "Tout simplement pour qu'en feuilletant le sujet\non ne s'apercoive pas de la nature de l'exo..\n";
+if ($argv[1] == "mais pourquoi cette demo ?")
+	echo "Tout simplement pour qu'en feuilletant le sujet\non ne s'apercoive pas de la nature de l'exo..\n";
 ?>
