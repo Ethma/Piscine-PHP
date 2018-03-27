867708724f865beeebf2e1fe65721a85b0a39b3f
mabessir <mabessir@student.42.fr>
2018-03-27 20:42:54 +0200

    Added doop2

--- Day01/ex11/do_op_2.php
+++ Day01/ex11/do_op_2.php
@@ -5,7 +5,7 @@ function error($i)
 	if ($i == 1)
 		echo "Incorrect Parameters\n";
 	if ($i == 2)
-		echo "Syntax Error";
+		echo "Syntax Error\n";
 	exit();
 }
 if ($argc != 2)
@@ -14,31 +14,102 @@ $str = $argv[1];
 $strr = trim($str);
 while (strpos($strr, "  ") == true)
 	$strr = str_replace("  ", " ", $strr);
-if ($array = explode("+", $strr))
+$test1 = $strr;
+$test2 = $strr;
+$test3 = $strr;
+$test4 = $strr;
+$test5 = $strr;
+if ($array = explode("+", $test1))
 {
-	echo $array[0] + $array[1];
-	return (0);
+	$test = count($array);
+	if ($test > 2)
+		error(2);
+	if ($test == 2)
+	{
+		$arg0 = trim($array[0]);
+		$arg1 = trim($array[1]);
+		if (is_numeric($arg0) && is_numeric($arg1))
+		{
+			echo $arg0 + $arg1."\n";
+			return (0);
+		}
+		else
+			error(2);
+	}
 }
-else if ($array = explode("-", $strr))
+if ($array = explode("-", $test2))
 {
-	echo $array[0] - $array[1];
-	return (0);
+	$test = count($array);
+	if ($test > 2)
+	{
+		echo "Syntax Error\n";
+		return (0);
+	}
+	if ($test == 2)
+	{
+		$arg0 = trim($array[0]);
+		$arg1 = trim($array[1]);
+		if (is_numeric($arg0) && is_numeric($arg1))
+		{
+			echo $arg0 - $arg1."\n";
+			return (0);
+		}
+		else
+			error(2);
+	}
 }
-else if ($array = explode("*", $strr))
+if ($array = explode("/", $test3))
 {
-	echo $array[0] * $array[1];
-	return (0);
+	$test = count($array);
+	if ($test > 2)
+		error(2);
+	if ($test == 2)
+	{
+		$arg0 = trim($array[0]);
+		$arg1 = trim($array[1]);
+		if (is_numeric($arg0) && is_numeric($arg1))
+		{
+			echo $arg0 / $arg1."\n";
+			return (0);
+		}
+		else
+			error(2);
+	}
 }
-else if ($array = explode("/", $strr))
+if ($array = explode("*", $test4))
 {
-	echo $array[0] / $array[1];
-	return (0);
+	$test = count($array);
+	if ($test > 2)
+		error(2);
+	if ($test == 2)
+	{
+		$arg0 = trim($array[0]);
+		$arg1 = trim($array[1]);
+		if (is_numeric($arg0) && is_numeric($arg1))
+		{
+			echo $arg0 * $arg1."\n";
+			return (0);
+		}
+		else
+			error(2);
+	}
 }
-else if ($array = explode("%", $strr))
+if ($array = explode("%", $test5))
 {
-	echo $array[0] % $array[1];
-	return (0);
+	$test = count($array);
+	if ($test > 2)
+		error(2);
+	if ($test == 2)
+	{
+		$arg0 = trim($array[0]);
+		$arg1 = trim($array[1]);
+		if (is_numeric($arg0) && is_numeric($arg1))
+		{
+			echo $arg0 % $arg1."\n";
+			return (0);
+		}
+	}
 }
-else
+if (1)
 	error(2);
 ?>
